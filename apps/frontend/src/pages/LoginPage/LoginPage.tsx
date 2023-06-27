dfe6e00d369e9fd6b705a06d80a3b4c079d6fb29
Paul PHP <93597135+PaulPHPE@users.noreply.github.com>
2023-06-27 14:57:09 +0200

    Feat/frontend creerloginpage (#3)
    * feat/frontend:Créer la login page
    
    * feat/frontend: login page, explode composant
    
    ---------
    
    Co-authored-by: leolezer <lezerleo7@gmail.com>

--- apps/frontend/src/pages/LoginPage/LoginPage.tsx
+++ apps/frontend/src/pages/LoginPage/LoginPage.tsx
@@ -1,37 +1,21 @@
 import { createSignal, type Component } from "solid-js";
 
+
+import "./LoginPage.scss";
+import ForgetPassword from "./components/ForgetPassword/ForgetPassword";
+import Login from "./components/Login/Login";
+
 const LoginPage: Component = () => {
-  const [username, setUsername] = createSignal("");
-  const [password, setPassword] = createSignal("");
-
-  const handleSubmit = () => {};
-
+  const [forgetPassword, setForgetPassword] = createSignal(false);
 
   return (
-    <form onSubmit={handleSubmit}>
-      <h2>Login</h2>
-      <div>
-        <label id="username">Username</label>
-        <input
-          id="username"
-          type="text"
-          value={username()}
-          onInput={() => {}}
-        />
-      </div>
-      <div>
-        <label id="password">Password</label>
-        <input
-          id="password"
-          type="password"
-          value={password()}
-          onInput={() => {}}
-        />
-      </div>
-      <div>
-        <button type="submit">Login</button>
-      </div>
-    </form>
+    <div class="container">
+      {forgetPassword() ? (
+        <ForgetPassword setForgetPassword={setForgetPassword} />
+      ) : (
+        <Login setForgetPassword={setForgetPassword} />
+      )}
+    </div>
   );
 };
 
