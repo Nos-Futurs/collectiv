02040accd77e52f9d40f5864f3da02fce14394e9
PaulPHPE <93597135+PaulPHPE@users.noreply.github.com>
2023-07-04 14:07:32 +0200

    feat(frontend): create layout with header and footer

---
+++ apps/frontend/src/components/Header/Header.tsx
@@ -0,0 +1,10 @@
+import type { Component } from "solid-js";
+import "./Header.scss"
+
+const Header: Component = () => {
+  return (
+    <section id="header">HEADER</section>
+  );
+};
+
+export default Header;
---
+++ apps/frontend/src/layout/Layout.tsx
@@ -0,0 +1,19 @@
+import { Component, JSXElement } from 'solid-js';
+import Footer from '../components/Footer/Footer';
+import Header from '../components/Header/Header';
+import './Layout.scss';
+
+interface HomeProps {
+  id: string;
+  children: JSXElement;
+}
+
+const PageLayout: Component<HomeProps> = (props: HomeProps) => (
+  <main id={props.id} class="page">
+    <Header />
+    <div class="content">{props.children}</div>
+    <Footer />
+  </main>
+);
+
+export default PageLayout;
