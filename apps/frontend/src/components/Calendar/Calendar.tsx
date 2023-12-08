042a4a1ae2cc9ad854d566db3ddfb6e44713a536
PaulPHPE <93597135+PaulPHPE@users.noreply.github.com>
2023-12-08 11:15:40 +0100

    feat: event container set up

--- apps/frontend/src/components/Calendar/Calendar.tsx
+++ apps/frontend/src/components/Calendar/Calendar.tsx
@@ -1,7 +1,8 @@
-import { createEffect, createSignal } from "solid-js";
+import { createSignal } from "solid-js";
 import "./calendar.scss";
 import Selector from "./components/Selector.jsx";
 import DateCalendar from "./components/DateCalendar.jsx";
+import EventContainer from "./components/EventCalendar.jsx";
 
 const Calendar = () => {
   const [year, setYear] = createSignal<number>(new Date().getFullYear());
@@ -12,7 +13,7 @@ const Calendar = () => {
     <div id="calendar">
       <Selector year={year} setYear={setYear} month={month} setMonth={setMonth} />
       <DateCalendar year={year} month={month}  date={date} setDate={setDate} />
-      <div id="event-container">Event</div>
+      <EventContainer date={date}/>
     </div>
   );
 };
